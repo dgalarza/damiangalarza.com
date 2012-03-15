79d4d83835257925ae56277107c66beaa860c022
Damian Galarza <galarza.d@gmail.com>
2012-03-14 20:15:49 -0700

    Ability to add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,16 @@
 class CommentsController < ApplicationController
   def new
+  end
 
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment])
+    if @comment.save
+      flash[:notice] = "Thanks for your comment!"
+    else
+      flash[:error] = "An error occurred while trying to save your comment."
+    end
+
+    redirect_to @post
   end
 end
