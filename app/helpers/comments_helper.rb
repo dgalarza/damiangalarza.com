1fa86c6d148cd1b4604b7c00a9ad60f646b8c7e3
Damian Galarza <galarza.d@gmail.com>
2012-03-15 05:33:32 -0700

    Updating gravatar request
    *ensure pg rating
    
    * 60x60 images

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
   def gravatar(email)
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
+    "//gravatar.com/avatar/#{gravatar_id}.png?s=60r=pg"
   end
 end
