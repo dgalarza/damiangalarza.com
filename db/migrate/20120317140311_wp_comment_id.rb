e5928c011dfc2ab35c54199525080ca59230c0d4
Damian Galarza <galarza.d@gmail.com>
2012-03-19 10:43:04 -0700

    Missed a broken migration

--- db/migrate/20120317140311_wp_comment_id.rb
+++
@@ -1,9 +0,0 @@
-class WpCommentId < ActiveRecord::Migration
-  def up
-    add_column :comments, :wp_id, :integer, :null => true
-  end
-
-  def down
-    remove_column :comments, :wp_id
-  end
-end
