2015f1005ccd2d777ba2c85d9410089face5d245
Damian Galarza <galarza.d@gmail.com>
2012-03-21 13:47:05 -0400

    Disabling assets is bad'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,6 @@ DamiangalarzaCom::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.enabled = false
-
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
