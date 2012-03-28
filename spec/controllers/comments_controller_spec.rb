445e4ce3343259b8661da272c9a808f2dbd8745c
Damian Galarza <galarza.d@gmail.com>
2012-03-27 19:06:19 -0700

    Testing comment creation redirect

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe CommentsController do
 
+  def do_create
+    @post = FactoryGirl.create(:post)
+    post :create, :post_id => @post.id
+  end
+
+  it "should redirect back to blog post" do
+    do_create
+    response.should redirect_to( blog_permalink_path(:category => @post.category.name.downcase, :slug => @post.slug) )
+  end
+
 end
