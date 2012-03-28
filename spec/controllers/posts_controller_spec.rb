285099caa14a0766d1d700018ffd3778419951b6
Damian Galarza <galarza.d@gmail.com>
2012-03-27 19:06:34 -0700

    Updating spec for blog permalink route

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,8 +4,8 @@ describe PostsController do
 
   describe "Routes" do
 
-    it "should route to post by date and post slug" do
-      assert_routing("/posts/2011/1/25/post-title", :controller => 'posts', :action => 'show', :year => '2011', :month => '1', :day => '25', :slug => 'post-title')
+    it "should route to post by category and post slug" do
+      assert_routing("/posts/javascript/post-title", :controller => 'posts', :action => 'show', :method => :get, :category => 'javascript', :slug => 'post-title')
     end
 
   end
