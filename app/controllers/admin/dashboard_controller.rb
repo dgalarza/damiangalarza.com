86685d4f81248f1ddaede33cd1e44d29d03a82b8
Damian Galarza <galarza.d@gmail.com>
2012-03-30 18:38:35 -0700

    Fixing order for posts in admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < Admin::AdminController
   def index
-    @posts = Post.admin_table_fields.includes(:category).limit(5)
+    @posts = Post.admin_table_fields.order('created_at DESC').includes(:category).limit(5)
     @comments = Comment.unscoped.admin_table_fields.order("created_at DESC").limit(10)
   end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PostsController < Admin::AdminController
   def index
-    @posts = Post.admin_table_fields.includes(:category).limit(25)
+    @posts = Post.admin_table_fields.includes(:category).order('created_at DESC').limit(25)
   end
 
   def edit
