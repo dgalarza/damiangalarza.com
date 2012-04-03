017d3a57a71d39e4126bfedaff6739f6271e145e
Damian Galarza <galarza.d@gmail.com>
2012-04-02 17:00:39 -0700

    Display an admin link when logged in as admin

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -12,5 +12,26 @@ describe ApplicationHelper do
 
     post_permalink_path(post).should == "/posts/javascript/my-post"
   end
-  
+
+  describe "display_admin?" do
+
+    it "should not display admin if no user is logged in" do
+      helper.stub(:current_user).and_return(nil)
+      helper.display_admin?.should be_false
+    end
+
+    specify "display_admin if the user is an admin" do
+      user = FactoryGirl.create(:user, :admin => true)
+      helper.stub(:current_user).and_return(user)
+      helper.display_admin?.should be_true
+    end
+
+    specify "display_admin should be false if user is not an admin" do
+      user = FactoryGirl.create(:user, :admin => false)
+      helper.stub(:current_user).and_return(user)
+      helper.display_admin?.should be_false
+    end
+
+  end
+
 end
