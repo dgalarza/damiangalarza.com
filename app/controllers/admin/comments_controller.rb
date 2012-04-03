530923d8efac90f27be82cb1c31f6163477350d3
Damian Galarza <galarza.d@gmail.com>
2012-04-02 18:58:54 -0700

    Update to comments

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,29 +3,28 @@ class Admin::CommentsController < Admin::AdminController
   before_filter :find_comment, :only => [:mark_as_spam, :unmark_as_spam]
 
   def index
-    @comments = Comment.unscoped.admin_table_fields.order('created_at DESC')
+    @comments = Comment.admin_table_fields.order('created_at DESC')
   end
 
   def edit
-    @comment = Comment.unscoped.find params[:id]
+    @comment = Comment.find params[:id]
   end
 
   def destroy
     @comment = Comment.find params[:id]
     @comment.delete
     
-    flash[:notice] = 'Comment deleted'
-    redirect_to :admin_comments
+    render :json => { :status => 'Success' }
   end
 
   def spam
-    @comment = Comment.unscoped.find params[:comment_id]
+    @comment = Comment.find params[:comment_id]
     @comment.mark_as_spam
     render :json => { :status => "Success" }
   end
 
   def unspam
-    @comment = Comment.unscoped.find params[:comment_id]
+    @comment = Comment.find params[:comment_id]
     @comment.unmark_as_spam
     render :json => { :status => "Success" }
   end
