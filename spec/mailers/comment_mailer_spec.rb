6b5edc9c295888d0b5da78fb98c0fb4dbe4969f2
Damian Galarza <galarza.d@gmail.com>
2012-04-13 10:35:25 -0400

    Specs for comment mailer

--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe CommentMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Comment Notification" do
+    let (:comment) { FactoryGirl.create(:comment) }
+    let (:mail) { CommentMailer.comment_notification(comment) }
+
+    it "should have a subject of Comment Received" do
+      mail.should have_subject /Comment Received/
+    end
+
+    specify "body should include comment" do
+      mail.should have_body_text /#{comment.comment}/
+    end
+
+    specify "body should include the author" do
+      mail.should have_body_text /#{comment.username}/
+    end
+  end
+
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include EmailSpec::Helpers
+  include EmailSpec::Matchers
+
+  it "should notify admin of new approved comments" do
+    comment = FactoryGirl.build(:comment)
+    mailer = double('mailer')
+    mailer.stub(:deliver)
+
+    CommentMailer.should_receive(:comment_notification).with(comment).and_return(mailer)
+    comment.save
+  end
+
+  it "should not notify of known spam comments" do
+    comment = FactoryGirl.build(:comment, :approved => false)
+    comment.stub(:check_for_spam).and_return(true)
+
+    CommentMailer.should_not_receive(:comment_notification)
+    comment.save
+  end
+
 end
